34f7895dfe1691077c03e65416ccbaa81229cad6
Mhmd <38478286+ZaaWi@users.noreply.github.com>
2022-01-04 15:21:24 +0200

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import csv
 import json
 
+rowOneHeader = "row one"
+rowTwoHeader = "row two"
+
 with open('data.json', encoding="utf-8") as jsonFile:
     data = json.load(jsonFile)
 
 with open('output.csv', 'w', encoding="utf-8") as output:
-    print(output)
     writer = csv.writer(output)
-    writer.writerow(['key', 'translation'])
+    writer.writerow([rowOneHeader, rowTwoHeader])
     for item in data:
-        print(item + data[item])
         writer.writerow([item, data[item]])
